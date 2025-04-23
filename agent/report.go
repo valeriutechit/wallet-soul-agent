00d7f45313a3f6c4162c139b4a8956d656803080
valerii <valerii.bodnarchuk@docfinder.at>
2025-04-23 03:04:55 +0200

    return camelCase

--- agent/report.go
+++ agent/report.go
@@ -3,8 +3,8 @@ package agent
 import "wallet-soul-agent/utils"
 
 type SoulReport struct {
-	Address    string
-	Tokens     []utils.Token
-	Profile    string
-	Reflection string
+	Address    string `json:"address"`
+	Tokens     []utils.Token `json:"tokens"`
+	Profile    string `json:"profile"`
+	Reflection string `json:"reflection"`
 }
